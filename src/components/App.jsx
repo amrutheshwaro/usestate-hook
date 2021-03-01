dad513d8b1f999e92c29956f6790739e85ea0045
Amrutheshwar O <55506973+amrutheshwaro@users.noreply.github.com>
2021-03-01 23:11:34 +0530

    final commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
-function App() {
-  return <div />;
-}
+var App = function () {
+  const [count, setCount] = useState(0);
+
+  var increment = function () {
+    setCount(count + 1);
+  };
+
+  var decrement = function () {
+    count !== 0 && setCount(count - 1);
+  };
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={decrement}>-</button>
+      <button onClick={increment}>+</button>
+    </div>
+  );
+};
 
 export default App;
